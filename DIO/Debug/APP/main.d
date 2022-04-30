f061887d1591d5a0a89cd2a25e2e3ef5950f649e
ahmedmaged0a <ahmedmaged0a@gmail.com>
2022-04-30 21:38:56 +0200

    Atmega32 Drivers

--- DIO/Debug/APP/main.d
+++
@@ -1,8 +0,0 @@
-APP/main.o APP/main.o: ../APP/main.c ../APP/../LIB/errorState.h \
-  ../APP/../LIB/stdTypes.h ../APP/../MCAL/DIO/DIO_interface.h
-
-../APP/../LIB/errorState.h:
-
-../APP/../LIB/stdTypes.h:
-
-../APP/../MCAL/DIO/DIO_interface.h:
